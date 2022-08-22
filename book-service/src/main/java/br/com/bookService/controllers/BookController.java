b8b918c43bd7edceadebdeeeb46a90a7fcc056ac
dylandmr <dylandmrodrigues@hotmail.com>
2022-08-21 22:00:13 -0300

    Switching endpoint call to cambio-service from explicit call to proxy call via Feign using CambioProxy.

--- book-service/src/main/java/br/com/bookService/controllers/BookController.java
+++ book-service/src/main/java/br/com/bookService/controllers/BookController.java
@@ -3,22 +3,19 @@ package br.com.bookService.controllers;
 import br.com.bookService.models.Book;
 import br.com.bookService.proxy.CambioProxy;
 import br.com.bookService.repository.BookRepository;
-import br.com.bookService.response.CambioResponse;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import javax.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("book-service")
 public class BookController {
     private final Environment environment;
     private final BookRepository bookRepository;
-
     private final CambioProxy cambioProxy;
 
     public BookController(Environment environment, BookRepository bookRepository, CambioProxy cambioProxy) {
@@ -29,26 +26,18 @@ public class BookController {
 
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
-        var book = bookRepository.getReferenceById(id);
+        try {
+            var book = bookRepository.getReferenceById(id);
 
-        if (book == null) {
+            var cambioResponse = cambioProxy.getCambio(book.getPrice(), "USD", currency);
+            var currentPort = environment.getProperty("local.server.port");
+
+            book.setEnvironment(currentPort + " feign");
+            book.setPrice(cambioResponse.getConvertedValue());
+
+            return book;
+        } catch (EntityNotFoundException exception) {
             throw new RuntimeException("Book not found.");
         }
-
-        var params = new HashMap<String, String>();
-        params.put("amount", book.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
-
-        var cambioResponse = new RestTemplate().getForEntity(
-            "http://localhost:8000/cambio-service/{amount}/{from}/{to}",
-            CambioResponse.class,
-            params
-        );
-
-        var currentPort = environment.getProperty("local.server.port");
-        book.setEnvironment(currentPort);
-        book.setPrice(cambioResponse.getBody().getConvertedValue());
-        return book;
     }
 }
--- book-service/src/main/java/br/com/bookService/proxy/CambioProxy.java
+++ book-service/src/main/java/br/com/bookService/proxy/CambioProxy.java
@@ -5,14 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
-
 @FeignClient(name = "cambio-service", url = "localhost:8000")
 public interface CambioProxy {
 
-    @GetMapping("'/cambio-service/{amount}/{from}/{to}")
-    public CambioResponse getCambio(
-            @PathVariable("amount") BigDecimal amount,
+    @GetMapping("/cambio-service/{amount}/{from}/{to}")
+    CambioResponse getCambio(
+            @PathVariable("amount") Double amount,
             @PathVariable("from") String from,
             @PathVariable("to") String to);
 }
